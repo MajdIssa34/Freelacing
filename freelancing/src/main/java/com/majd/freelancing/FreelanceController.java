144ed54c001b38848dde49de8c10fa26daeeaf9d
MajdIssa34 <issamajd00@gmail.com>
2024-12-28 17:27:04 +1100

    Nothing major

---
+++ freelancing/src/main/java/com/majd/freelancing/FreelanceController.java
@@ -0,0 +1,12 @@
+package com.majd.freelancing;
+
+import org.apache.catalina.filters.ExpiresFilter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FreelanceController {
+
+
+
+}
